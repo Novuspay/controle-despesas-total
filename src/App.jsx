58e24d7ea9c971ab45768e8a9640a96784af7ef4
Novuspay <contato@novuspay.com.br>
2025-06-06 23:16:46 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
+import NovaTransacao from './components/NovaTransacao';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/nova" element={<NovaTransacao />} />
       </Routes>
     </BrowserRouter>
   );
