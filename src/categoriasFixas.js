a2fd14937aa1a6b9bd3959a24f18d4646aa12bd5
Novuspay <contato@novuspay.com.br>
2025-06-07 20:49:47 -0300

    Create categoriasFixas.js

---
+++ src/categoriasFixas.js
@@ -0,0 +1,31 @@
+// src/categoriasFixas.js
+
+export const categoriasFixas = [
+  // ENTRADAS
+  { nome: 'Salários', tipo: 'entrada' },
+  { nome: 'Freelance', tipo: 'entrada' },
+  { nome: 'Aportes', tipo: 'entrada' },
+  { nome: 'Vendas', tipo: 'entrada' },
+  { nome: 'Presentes', tipo: 'entrada' },
+  { nome: 'Outros', tipo: 'entrada' },
+
+  // SAÍDAS
+  { nome: 'Alimentação', tipo: 'saida' },
+  { nome: 'Transporte', tipo: 'saida' },
+  { nome: 'Moradia', tipo: 'saida' },
+  { nome: 'Saúde', tipo: 'saida' },
+  { nome: 'Educação', tipo: 'saida' },
+  { nome: 'Lazer', tipo: 'saida' },
+  { nome: 'Roupa', tipo: 'saida' },
+  { nome: 'Pet', tipo: 'saida' },
+  { nome: 'Delivery', tipo: 'saida' },
+  { nome: 'Contas diversas', tipo: 'saida' },
+  { nome: 'Assinaturas e serviços', tipo: 'saida' },
+  { nome: 'Investimentos', tipo: 'saida' },
+  { nome: 'Dívidas/Empréstimos', tipo: 'saida' },
+  { nome: 'Impostos', tipo: 'saida' },
+  { nome: 'Tarifas bancárias', tipo: 'saida' },
+  { nome: 'Lazer e Hobbies', tipo: 'saida' },
+  { nome: 'Bares e Restaurantes', tipo: 'saida' },
+  { nome: 'Cuidados pessoais', tipo: 'saida' }
+];
